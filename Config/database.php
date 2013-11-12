5c03a9bb0607aceca82779b2faf14a706a81e7c3
SebastianGerard <ger.sal.jo@gmail.com>
2013-11-12 10:44:46 -0400

    cake

---
+++ Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $Proyecto2 = array(
+		'datasource' => 'Database/mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'port' => 80,
+		'login' => 'root',
+		'password' => '',
+		'database' => 'Proyecto2-Database',
+	);
+}
