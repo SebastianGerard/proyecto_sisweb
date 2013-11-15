3b21d60efd4a43da0a615c99104859c96084cf90
araoznelson <nelsonaraoz@ymail.com>
2013-11-15 17:59:59 -0400

    anadiendo validaciones

--- Model/Accessory.php
+++ Model/Accessory.php
@@ -1,6 +1,11 @@
 <?php
 class Accessory extends AppModel {
 public $hasMany = array('Artifact');
-
+	var $validate=array(
+			'name'=>array(
+				'required'=>true,
+				'message'=>'Invalid email'
+				)
+		);
 }
 ?>
--- Model/Room.php
+++ Model/Room.php
@@ -1,5 +1,27 @@
 <?php
 class Room extends AppModel {
 public $hasMany = array('RoomImage','Artifact');
+	var $validate=array(
+			'code'=>array(
+				'rule'=>'alphaNumeric',
+				'required'=>true,
+				'message'=>'Only alpha-numeric'
+				),
+			'type'=>array(
+				'rule'=>'alphaNumeric',
+				'required'=>true,
+				'message'=>'Only alpha-numeric'
+				),
+			'price'=>array(
+				'rule'=>'numeric',
+				'required'=>true,
+				'message'=>'Only numbers'
+				),
+				'capacity'=>array(
+				'rule'=>'Numeric',
+				'required'=>true,
+				'message'=>'Only numbers'
+				)
+		);
 }
 ?>
