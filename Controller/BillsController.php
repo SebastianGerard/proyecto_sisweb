43d2363ef79506539d4d777fe704535d6189705b
araoznelson <nelsonaraoz@ymail.com>
2013-12-06 12:25:57 -0400

    servicios

--- Controller/BillsController.php
+++ Controller/BillsController.php
@@ -11,7 +11,9 @@ class BillsController extends AppController {
     	{
     		Controller::loadModel('Service');
     		Controller::loadModel('User');
- 			$this->set('services', $this->User->Service->find('all',array("conditions" => array('Service.user_id'=>$id))));
+            $this->Service->query("update services set active=0 where user_id=$id");
+ 			$this->set('services', $this->User->Service->find('all',array("conditions" => array('Service.user_id'=>$id),array('active'=>1))));
+            
             $this->set('users', $this->User->find('all',array("conditions" => array('User.id'=>$id))));
     	}
     	
